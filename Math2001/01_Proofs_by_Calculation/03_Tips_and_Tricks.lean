35647c60991f45d5233770d9cef8473e88594c13
csvaughen <csvaughen@gmail.com>
2025-02-01 09:44:42 -0500

    removed some examples, final version lean2-hw

--- Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
+++ Math2001/01_Proofs_by_Calculation/03_Tips_and_Tricks.lean
@@ -56,38 +56,7 @@ Solve these problems yourself.  You may find it helpful to solve them on paper b
 up in Lean. -/
 
 
-example {x y : ℝ} (h1 : x = 3) (h2 : y = 4 * x - 3) : y = 9 :=
-  sorry
-
 example {a b : ℤ} (h : a - b = 0) : a = b :=
-  sorry
-
-example {x y : ℤ} (h1 : x - 3 * y = 5) (h2 : y = 3) : x = 14 :=
-  sorry
-
-example {p q : ℚ} (h1 : p - 2 * q = 1) (h2 : q = -1) : p = -1 :=
-  sorry
-
-example {x y : ℚ} (h1 : y + 1 = 3) (h2 : x + 2 * y = 3) : x = -1 :=
-  sorry
-
-example {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 :=
-  sorry
-
-example {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3)
-    (h3 : c = 1) : a = 2 :=
-  sorry
-
-example {u v : ℚ} (h1 : 4 * u + v = 3) (h2 : v = 2) : u = 1 / 4 :=
-  sorry
-
-example {c : ℚ} (h1 : 4 * c + 1 = 3 * c - 2) : c = -3 :=
-  sorry
-
-example {p : ℝ} (h1 : 5 * p - 3 = 3 * p + 1) : p = 2 :=
-  sorry
-
-example {x y : ℤ} (h1 : 2 * x + y = 4) (h2 : x + y = 1) : x = 3 :=
   sorry
 
 example {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 :=
@@ -98,9 +67,6 @@ example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 :=
 
 example {t : ℚ} (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 :=
-  sorry
-
-example {x y : ℝ} (h1 : x + 3 = 5) (h2 : 2 * x - y * x = 0) : y = 2 :=
   sorry
 
 example {p q r : ℚ} (h1 : p + q + r = 0) (h2 : p * q + p * r + q * r = 2) :
